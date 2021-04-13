c48725cb778c7de147627150939501bc3185c1c4
Rickert Huismanius <70889425+99062653@users.noreply.github.com>
2021-04-13 12:33:55 +0200

    0.54

--- includes/content.php
+++ includes/content.php
@@ -1,7 +1,9 @@
 <?php
     //$conn = naam van db
     $stmt = $conn->query("SELECT * FROM characters");
-    
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+
 ?>
 <html>
     <div id="content">
@@ -10,10 +12,11 @@
             ?>
             <div class="character">
                 <div class="left">
-                    <img class="avatar" src="#" alt="Avatar">
+                    <img class="avatar" src="./images/<?php echo $row["avatar"]; ?>" alt="Avatar">
                 </div>
                 <div class="right">
-                    <h2 class="naam"></h2>
+                    <h2 class="naam"><?php echo $row["name"]; ?></h2>
+                    <a class="knop" href="./pagina/character.php?name=<?php echo $row['name'];?>">Bekijk</a>
                 </div>
             </div>
             <?php
--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,5 @@
 <html>
     <footer>&copy; Rick Huisman <?php echo date("Y") ?></footer>
 </html>
+
+<?php //https://www.youtube.com/watch?v=kEW6f7Pilc4&t=1710s Dankjewel naar deze vent ?>
